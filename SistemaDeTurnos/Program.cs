880351ee4bdab82cc35ff1ab6118b8acc654ef03
Franco <francoherrera77003@gmail.com>
2022-11-28 21:36:46 -0300

    Creo el menú principal con sus respectivas opciones

--- SistemaDeTurnos/Program.cs
+++ SistemaDeTurnos/Program.cs
@@ -10,6 +10,24 @@ namespace SistemaDeTurnos
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Bienvenido");
+
+            Console.WriteLine("-------------------------------------------------------------");
+
+            Console.WriteLine("Que desea hacer?");
+
+            Console.WriteLine("-------------------------------------------------------------");
+
+            Console.WriteLine("1. Dar un turno");
+            Console.WriteLine("2. Actualizar diagnóstico de un paciente");
+            Console.WriteLine("3. Cancelar un turno");
+            Console.WriteLine("4. Ver turnos");
+            Console.WriteLine("5. Ver obras sociales");
+            Console.WriteLine("6. Agregar paciente");
+            Console.WriteLine("7. Eliminar paciente");
+            Console.WriteLine("8. Ver pacientes");
+
+            Console.ReadKey();
         }
     }
 }
