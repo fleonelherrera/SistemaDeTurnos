bd41a50a76331454f7304360730aa470801fcb9a
Franco <francoherrera77003@gmail.com>
2022-11-28 23:12:50 -0300

    Le di funcionalidad al menú con un switch, cree las clases persona y obraSocial

---
+++ SistemaDeTurnos/ObraSocial.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaDeTurnos
+{
+    internal class ObraSocial
+    {
+        public string Nombre { get; set; }
+        public int NroAfiliado { get; set; }
+    }
+}
--- SistemaDeTurnos/Program.cs
+++ SistemaDeTurnos/Program.cs
@@ -14,10 +14,6 @@ namespace SistemaDeTurnos
 
             Console.WriteLine("-------------------------------------------------------------");
 
-            Console.WriteLine("Que desea hacer?");
-
-            Console.WriteLine("-------------------------------------------------------------");
-
             Console.WriteLine("1. Dar un turno");
             Console.WriteLine("2. Actualizar diagnóstico de un paciente");
             Console.WriteLine("3. Cancelar un turno");
@@ -26,6 +22,42 @@ namespace SistemaDeTurnos
             Console.WriteLine("6. Agregar paciente");
             Console.WriteLine("7. Eliminar paciente");
             Console.WriteLine("8. Ver pacientes");
+
+            Console.WriteLine("-------------------------------------------------------------");
+
+            Console.WriteLine("Que desea hacer?");
+            string opcion = Console.ReadLine();
+
+            switch (opcion)
+            {
+                case "1":
+                    Console.WriteLine("opción 1");
+                    break;
+                case "2":
+                    Console.WriteLine("opcion 2");
+                    break;
+                case "3":
+                    Console.WriteLine("opcion 3");
+                    break;
+                case "4":
+                    Console.WriteLine("opcion 4");
+                    break;
+                case "5":
+                    Console.WriteLine("opcion 5");
+                    break;
+                case "6":
+                    Console.WriteLine("opcion 6");
+                    break;
+                case "7":
+                    Console.WriteLine("opcion 7");
+                    break;
+                case "8":
+                    Console.WriteLine("opcion 8");
+                    break;
+                default:
+                    Console.WriteLine("Opción incorrecta");
+                    break;
+            }
 
             Console.ReadKey();
         }
